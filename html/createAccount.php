d97bf085f3fe278479b20e1dafe0e532fd54542c
viman39 <41690510+viman39@users.noreply.github.com>
2019-04-27 19:41:21 +0300

    bug fixing

--- html/createAccount.php
+++ html/createAccount.php
@@ -7,43 +7,45 @@
     </head>
     <body>
         <h1>SignUp</h1>
-        <div class="formular">
-            <div>
-                <label for="email"><b>Email: </b></label>
-                <input type="text" id="email" placeholder="email address" name="emailAddress" required>
-            </div>
+        <form action="createAccount.php" method="POST">
+            <div class="formular">
+                <div>
+                    <label for="email"><b>Email: </b></label>
+                    <input type="text" id="email" placeholder="email address" name="emailAddress" required>
+                </div>
+        
+                <div>
+                    <label for="firstName"><b>First name: </b></label>
+                    <input type="text" id="firstName" placeholder="first name" name="firstName" required>
+                </div>
 
-            <div>
-                <label for="firstName"><b>First name: </b></label>
-                <input type="text" id="firstName" placeholder="first name" name="firstName" required>
-            </div>
+                <div>
+                <label for="lastName"><b>Last name: </b></label>
+                <input type="text" id="lastName" placeholder="last name" name="lastName" required>
+                </div>
 
-            <div>
-             <label for="lastName"><b>Last name: </b></label>
-             <input type="text" id="lastName" placeholder="last name" name="lastName" required>
-            </div>
+                <div>
+                    <label for="password"><b>Password: </b></label>
+                    <input type="password" id="password" placeholder="password" name="password" required>
+                </div>
 
-            <div>
-                <label for="password"><b>Password: </b></label>
-                <input type="password" id="password" placeholder="password" name="password" required>
-            </div>
+                <div>
+                    <label for="repeatPassword"><b>Repeat password: </b></label>
+                    <input type="password" id="repeatPassword" placeholder="repeat password" name="repeatPassword" required>
+                </div>
 
-            <div>
-                <label for="repeatPassword"><b>Repeat password: </b></label>
-                <input type="password" id="repeatPassword" placeholder="repeat password" name="repeatPassword" required>
+                <div>
+                    <label for="address"><b>address</b></label>
+                    <input type="text" id="address" placeholder="address" name="address" required>
+                </div>
             </div>
-
             <div>
-                <label for="address"><b>address</b></label>
-                <input type="text" id="address" placeholder="address" name="address" required>
+                <input type="submit">
+                <a class="cancelButton" href="index.html">Cancel</a>
             </div>
-        </div>
-        <div>
-            <a class="signUpButton" href="index.html">SignUp</a>
-            <a class="cancelButton" href="index.html">Cancel</a>
-        </div>
-        <div>
-            <p>By creating an account you agree to our <a href="tp.html" style="color:blue">Terms & Privacy</a>.</p>
-        </div>
+            <div>
+                <p>By creating an account you agree to our <a href="tp.html" style="color:blue">Terms & Privacy</a>.</p>
+            </div>
+        </form>
     </body>
 </html>
