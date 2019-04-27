5a841ab1872bcdb25cfb22af0e4db9550400da4e
viman39 <41690510+viman39@users.noreply.github.com>
2019-04-27 17:44:12 +0300

    create account back-end

--- back_end/createAccount.php
+++ back_end/createAccount.php
@@ -6,4 +6,74 @@
  * Time: 1:04 PM
  */
 
-include_once ('../html/createAccount.php');
+include_once ('../database/config.php');
+include_once ('../database/database.php');
+
+$errors = array(
+    'email' => '',
+    'name' => '',
+    'surname' => '',
+    'password' => '',
+    'repeatPassword' => '',
+    'address' => ''
+);
+
+$email = '';
+$name = '';
+$surname = '';
+$password = '';
+$secondPassword = '';
+$address = '';
+
+if(isset($_POST['emailAddress']) and !empty($_POST['emailAddress'])){
+    if(isset($_POST['firstName']) and !empty($_POST['firstName'])){
+        if(isset($_POST['lastName']) and !empty($_POST['lastName'])){
+            if(isset($_POST['password']) and !empty($_POST['password'])){
+                if(isset($_POST['repeatPassword']) and !empty($_POST['repeatPassword'])){
+                    if(isset($_POST['address']) and !empty($_POST['address'])){
+                        $email = $_POST['emailAddress'];
+                        $name = $_POST['firstNAme'];
+                        $surname = $_POST['lastName'];
+                        $password = $_POST['password'];
+                        $secondPassword = $_POST['repeatPassword'];
+                        $address = $_POST['address'];
+                        if(strpos($email, '@')){
+                            if($password == $secondPassword){
+                                if(strlen($password) > 5){
+                                    $conn = DB::getConnection(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+                                    $stmt = $conn->prepare('INSERT INTO clienti(id_client, adresa, email, nume, prenume, parola) VALUES(?, ?, ?, ?, ?, ?);');
+                                    $stmt->bind_param('isssss', $conn->insert_id, $address, $email, $name, $surname, $password);
+                                    $check = $stmt->execute();
+
+                                    if(! $check){
+                                        echo 'Database error!';
+                                    }
+
+                                    $stmt->close();
+                                } else{
+                                    $errors['password'] = 'Your password should be at least 6 characters long!';
+                                }
+                        } else{
+                                $errors['password'] = 'Passwords don\'t correspond!';
+                            }
+                        } else{
+                            $errors['email'] = 'Invalid email address';
+                        }
+                    } else{
+                        $errors['address'] = "Please insert your address!";
+                    }
+                } else{
+                    $errors['repeatPassword'] = 'Please insert your password!';
+                }
+            } else{
+                $errors['password'] = 'Please insert your password!';
+            }
+        } else{
+            $errors['surname'] = 'Please insert your last name!';
+        }
+    } else{
+        $errors['name'] = 'Please insert your first name!';
+    }
+} else{
+    $errors['email'] = 'Please insert your email address!';
+}
