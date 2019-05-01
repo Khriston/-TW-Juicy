c4e78f6b75497db5521ff18762ae1ebf58366b4d
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-01 11:43:32 +0300

    clean code

---
+++ frontend/index.php
@@ -0,0 +1,23 @@
+<?php include_once('menu.php')?>
+<!DOCTYPE html>
+<html lang="ro">
+
+<head>
+    <meta charset="utf-8">
+    <title>Juicy</title>
+    <link rel="stylesheet" href="../css/main.css"> 
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+
+<body>
+    <h1>Juicy</h1>
+
+    <h2>Welcome !</h2>
+    <p id="p1">Using our web service you can preview drinks that are available for purchase.</p>
+    <p id="p2">We also provide information regarding the respective drinks, such as price, the energy value, ingredients. </p>
+
+    <footer>Project by UAIC Students</footer>
+
+</body>
+
+</html>
