a10bf2f6738c19b66dfc53b7d76d46a89442684a
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-01 11:44:02 +0300

    menu.php

---
+++ frontend/menu.php
@@ -0,0 +1,11 @@
+<?php include_once('login.php') ?>
+<nav>
+    <ul id="menu">
+        <li><a class="active">Home</a></li>
+        <li><a href="catalog.html">Shop</a></li>
+        <li><a href="specialOffers.html">Special offers</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="#id01" onclick="document.getElementById('id01').style.display='block'">Login</a></li>
+        <li><a href="cart.html"><img src="../img/cart_transparent.png" alt="Shopping Cart"></a></li>
+    </ul>
+</nav>
