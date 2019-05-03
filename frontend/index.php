ade1e47a32e1044ee4fb6407398481697f727df7
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-03 16:20:13 +0300

    addSeller

--- frontend/index.php
+++ frontend/index.php
@@ -1,4 +1,5 @@
-<?php include_once('menu.php')?>
+<?php include_once('menu.php');
+      include_once('../backend/addSeller.php');?>
 <!DOCTYPE html>
 <html lang="ro">
 
@@ -15,6 +16,16 @@
     <h2>Welcome !</h2>
     <p id="p1">Using our web service you can preview drinks that are available for purchase.</p>
     <p id="p2">We also provide information regarding the respective drinks, such as price, the energy value, ingredients. </p>
+
+    <form action="../backend/addSeller.php" method="POST">
+        <div>
+            <h1>Become a seller! </h1>
+            <input type="text" id="addSeller" placeholder="your email here" name="emailSeller">
+        </div>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
 
     <footer>Project by UAIC Students</footer>
 
