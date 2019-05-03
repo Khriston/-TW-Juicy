9c876778e27e5b3a3a6cd56ad57e7701bc9de0e4
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-03 16:31:03 +0300

    bug fix

--- backend/login.php
+++ backend/login.php
@@ -19,18 +19,33 @@ if(isset($_POST['email']) && !empty($_POST['email'])){
         session_start();
         $email = $_POST['email'];
         $password = $_POST['passd'];
-        $password = md5($password);
+        $clienti = 'clienti';
+
         $conn = DB::getConnection(  DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
-        $stmt = $conn->prepare("SELECT * FROM clienti WHERE email=? and parola=?;");
-        $stmt->bind_param('ss', $email, $password);
+        $stmt = $conn->prepare("SELECT * FROM ? WHERE email=? and parola=?;");
+        $stmt->bind_param('sss', $clienti, $email, $password);
         $stmt->execute();
         $result = $stmt->get_result();
+
         if($result->num_rows > 0){
             $_SESSION['loggedin'] = true;
             $_SESSION['username'] = $email;
             $_SESSION['seller'] = false;
         } else{
-            $errors['email'] = 'Wrong email or password!';
+            $vanzator = 'vanzator';
+            $stmt->bind_param('sss', $vanzator, $email, $password);
+            $stmt->execute();
+            $result = $stmt->get_result();
+
+            if($result->num_rows > 0){
+                $_SESSION['loggedin'] = true;
+                $_SESSION['username'] = $email;
+                $_SESSION['seller'] = true;
+            }
+        }
+
+        if($_SESSION['loggedin'] == false) {
+            $errors = 'Wrong password or email';
         }
     } else{
         $errors['passd'] = 'Please insert your password!';
