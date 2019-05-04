603a9658b531e6d1c5ae2b8f3988e10327b2a2e4
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-04 15:01:33 +0300

    bug fix

--- frontend/menu.php
+++ frontend/menu.php
@@ -1,8 +1,8 @@
 <?php include_once('login.php') ?>
 <nav>
     <ul id="menu">
-        <li><a class="active">Home</a></li>
-        <li><a href="catalog.html">Shop</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="../backend/catalogGenerator.php">Shop</a></li>
         <li><a href="specialOffers.html">Special offers</a></li>
         <li><a href="contact.html">Contact</a></li>
         <li><a href="#id01" onclick="document.getElementById('id01').style.display='block'">Login</a></li>
