d1bf2ac10120db29bcaa99305edd3482e0a30650
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-16 09:31:30 +0300

    new cart generator is much more efficient

--- backend/catalogGenerator.php
+++ backend/catalogGenerator.php
@@ -1,78 +1,65 @@
+<!DOCTYPE html>
+<html lang="ro">
+
 <?php
 /**
  * Created by PhpStorm.
  * User: Lenovo
- * Date: 5/4/2019
- * Time: 9:51 AM
+ * Date: 5/16/2019
+ * Time: 8:18 AM
  */
 
-include_once ('../database/config.php');
-include_once ('../database/database.php');
+include_once('../database/config.php');
+include_once('../database/database.php');
+include_once('../frontend/menu.php');
 
-if($_SESSION['loggedin'] == 1) {
-    $file = "../frontend/catalog.php";
-    $catalog = fopen($file, 'w');
-
-    $title = "
-        <?php include_once('menu.php');?>
-        <!DOCTYPE html>
-        <html lang=\"ro\">
-        <head>
-            <meta charset=\"utf-8\">
-            <link rel=\"stylesheet\" type=\"text/css\" href=\"../css/catalog.css\">
-            <title>Juicy</title>
-            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
-            <link rel=\"stylesheet\" href=\"../css/login.css\">
-        </head>
-        <body>
-            <header>
-                <h1>Catalog sucuri</h1>
-            </header>
-            <div class=\"container\">
-                <div class=\"row\">
-                ";
-
-    fwrite($catalog, $title);
-
-    $conn = DB::getConnection(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
-    $stmt = $conn->prepare("SELECT * FROM produse;");
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    while ($row = $result->fetch_assoc()) {
-        $path = "../backend/products/" . $row['path_poza'];
-        $nume = $row['nume'];
-        $pret = $row['pret'];
-        $arome = $row['arome'];
-        $id_produs = $row['id_produs'];
-
-        $item = "
-                 <div class=\"column\">
-                        <div class=\"card\">
-                            <a class=\"button1\" href=\"DetaliiCola.html\">
-                                <img src=\"$path\" alt=\"$nume\" style=\"width:100%\">
-                            </a>
-                            <h2> $nume</h2>
-                            <p class=\"Pret\">$pret LEI</p>
-                            <p>$arome</p>
-                            <p><a href = \"../backend/addToCart.php?id_produs=$id_produs\" class = \"btn btn-primary\" role = \"button\">Adauga in cos</a></p>
-                        </div>
-                  </div>
-                  ";
-
-        fwrite($catalog, $item);
-    }
-
-    $end = "
-            </div>
-        </div>
-      </body>
-    </html>
-    ";
-
-    fwrite($catalog, $end);
-
-    header('Location: ../frontend/catalog.php');
-} else {
+/*if($_SESSION['loggedin'] == true)
     header("Location: ../frontend/index.php");
-}
+*/
+?>
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/catalog.css">
+    <title>Juicy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+    <header>
+        <h1>Catalog sucuri</h1>
+    </header>
+    <div class="container">
+        <div class="row">
+            <?php
+                $conn = DB::getConnection(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+                $result = $conn->query("SELECT * FROM produse;");
+
+                while($row = $result->fetch_assoc()){
+                    $path = "\"" . "../backend/products/" . $row['path_poza'] . "\"";
+                    $nume = $row['nume'];
+                    $pret = $row['pret'];
+                    $arome = $row['arome'];
+                    $id_produs = $row['id_produs'];
+                    $adauga_in_cos = "\"" . "../backend/addToCart.php?id_produs=$id_produs" . "\"";
+
+                    ?>
+                    <div class="column">
+                        <div class="card">
+                            <a class="button">
+                                <img src=<?php echo $path?> alt=<?php echo $nume?> style="width:100%">
+                            </a>
+                            <h2> <?php echo $nume ?> </h2>
+                            <p class="Pret"><?php echo $pret ?> LEI </p>
+                            <p><?php echo $arome ?></p>
+                            <p><button class="btn btn-primary" formaction=<?php echo $adauga_in_cos?>>Adauga in cos</button></p>
+                        </div>
+                    </div>
+            <?php
+                }
+            ?>
+        </div>
+    </div>
+</body>
+</html>
+
