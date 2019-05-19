f1562c0dc1597880659f15a56a55083192853469
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-19 19:55:36 +0300

    update page paths

--- frontend/menu.php
+++ frontend/menu.php
@@ -1,11 +1,11 @@
 <?php include_once('login.php');?>
 <nav>
     <ul id="menu">
-        <li><a href="index.php">Home</a></li>
+        <li><a href="../frontend/index.php">Home</a></li>
         <li><a href="../backend/catalogGenerator.php">Shop</a></li>
-        <li><a href="specialOffers.html">Special offers</a></li>
-        <li><a href="contact.html">Contact</a></li>
-        <li><a href="#id01" onclick="document.getElementById('id01').style.display='block'">Login</a></li>
-        <li><a href="cart.html"><img src="../img/cart_transparent.png" alt="Shopping Cart"></a></li>
+        <li><a href="../frontend/specialOffers.html">Special offers</a></li>
+        <li><a href="../frontend/contact.html">Contact</a></li>
+        <li><a href="#login" onclick="document.getElementById('id01').style.display='block'">Login</a></li>
+        <li><a href="../backend/cartGenerator.php"><img src="../img/cart_transparent.png" alt="Shopping Cart"></a></li>
     </ul>
 </nav>
