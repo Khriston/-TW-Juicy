39dbb1c0c289970184558dc341c4834bb6c9c13a
viman39 <41690510+viman39@users.noreply.github.com>
2019-05-20 12:33:08 +0300

    update print sold out when products are out of stock

--- backend/cartGenerator.php
+++ backend/cartGenerator.php
@@ -1,110 +1,81 @@
 <!DOCTYPE html>
 <html lang="ro">
+
 <?php
 /**
  * Created by PhpStorm.
- * User: Gheoca Victor Manuel
- * Date: 5/14/2019
- * Time: 9:17 AM
+ * User: Lenovo
+ * Date: 5/16/2019
+ * Time: 8:18 AM
  */
 
 include_once('../database/config.php');
 include_once('../database/database.php');
-include_once('../frontend/menu.php')
+include_once('../frontend/menu.php');
+
+/*if($_SESSION['loggedin'] == true)
+    header("Location: ../frontend/index.php");
+*/
 ?>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" type="text/css" href="../css/cart.css">
-    </head>
-    <body>
-        <header>
-            <h1 class="main_title">Juicy</h1>
-        </header>
 
-        <div class="cart">
-            <div class="title">Your Cart</div>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="../css/catalog.css">
+    <title>Juicy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+<body>
+    <header>
+        <h1>Catalog sucuri</h1>
+    </header>
+    <div class="container">
+        <div class="row">
             <?php
-                if($_SESSION['loggedin'] == true) {
-                    $conn = DB::getConnection(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
-                    $user_name = $_SESSION['username'];
-                    $stmt = $conn->prepare('SELECT id_client FROM clienti WHERE email=?;');
-                    $stmt->bind_param('s', $user_name);
+                $conn = DB::getConnection(DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+                $result_produse = $conn->query("SELECT * FROM produse;");
+
+                while($row = $result_produse->fetch_assoc()){
+                    $path = "\"" . "../backend/products/" . $row['path_poza'] . "\"";
+                    $nume = $row['nume'];
+                    $pret = $row['pret'];
+                    $arome = $row['arome'];
+                    $id_produs = $row['id_produs'];
+                    $adauga_in_cos = "\"" . "../backend/addToCart.php?id_produs=$id_produs" . "\"";
+
+                    $stmt = $conn->prepare("SELECT * FROM detine WHERE id_produs=?;");
+                    $stmt->bind_param('i', $id_produs);
                     $stmt->execute();
                     $result = $stmt->get_result();
                     $row = $result->fetch_assoc();
-                    $id_client = $row['id_client'];
+                    $quantity = $row['cantitate'];
                     $result->close();
-                    $stmt->close();
 
-                    $stmt = $conn->prepare("SELECT id_lista_cumparaturi FROM plateste_pentru WHERE id_client=?;");
-                    $stmt->bind_param('i', $id_client);
-                    $stmt->execute();
-                    $result = $stmt->get_result();
-                    $row = $result->fetch_assoc();
-                    $id_lista_cumparaturi = $row['id_lista_cumparaturi'];
-                    $result->close();
-                    $stmt->close();
-
-                    $stmt = $conn->prepare("SELECT * FROM cantitate_cumparata WHERE id_lista_cumparaturi=?;");
-                    $stmt->bind_param('i', $id_lista_cumparaturi);
-                    $stmt->execute();
-                    $result_lista_cumparaturi = $stmt->get_result();
-
-                    while ($row = $result_lista_cumparaturi->fetch_assoc()) {
-                        $id_produs = $row['id_produs'];
-                        $quantity = "\"" . $row['cantitate'] . "\"";
-
-                        $stmt->close();
-                        $stmt = $conn->prepare("SELECT * FROM produse WHERE id_produs=?;");
-                        $stmt->bind_param('i', $id_produs);
-                        $stmt->execute();
-                        $result = $stmt->get_result();
-                        $row = $result->fetch_assoc();
-
-                        $name = $row['nume'];
-                        $price = $row['pret'];
-                        $arome = $row['arome'];
-                        $img_path = "\"" . "../backend/products/" . $row['path_poza'] . "\"";
-                        $sour = $row['acidulat'] == 1 ? 'acidulat' : 'neacidulat';
-                        ?>
-                        <html>
-                        <div class="item">
-                            <div class="buttons">
-                                <span class="delete-btn"></span>
-                                <span class="like-btn"></span>
-                            </div>
-
-                            <div class="image">
-                                <img src=<?php echo $img_path ?> alt=""/>
-                            </div>
-
-                            <div class="description">
-                                <span><?php echo $name ?></span>
-                                <span><?php echo $arome ?></span>
-                                <span><?php echo $sour ?></span>
-                            </div>
-
-                            <div>
-                                <input type="number" name="quantity" value=<?php echo $quantity ?>>
-                            </div>
-
-                            <div class="total-price"><?php echo $price ?> Lei</div>
-                        </html>
-                        <?php
-                    }
-                } else{
-                    header("Location: ../frontend/index.php");
+                    $sold_out = $quantity == 0 ? true : false;
+                    ?>
+                    <div class="column">
+                        <div class="card">
+                            <a class="button">
+                                <img src=<?php echo $path?> alt=<?php echo $nume?> style="width:100%">
+                            </a>
+                            <h2> <?php echo $nume ?> </h2>
+                            <?php if($sold_out == false) {
+                                ?> <p class="Pret"><?php echo $pret ?> LEI </p> <?php
+                            } else{
+                                ?> <p class="soldOut">SOLD OUT</p> <?php
+                            }
+                            ?>
+                            <p><?php echo $arome ?></p>
+                            <?php if($sold_out == false) {
+                                ?> <p><a class="btn btn-primary" href=<?php echo $adauga_in_cos?>>Adauga in cos</a></p> <?php
+                            } ?>
+                        </div>
+                    </div>
+            <?php
                 }
             ?>
         </div>
-        <div class="checkout-btn">
-            <span><a href="../frontend/checkout.php">Check out</a></span>
-        </div>
-        </div>
-
-        <footer>Project by UAIC Students</footer>
-    </body>
+    </div>
+</body>
 </html>
-
 
